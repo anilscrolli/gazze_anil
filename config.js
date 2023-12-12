1c7f55eb5276c42726225fb66b281527a6762ed0
ScrolliStudio <152308836+ScrolliStudio@users.noreply.github.com>
2023-12-13 00:53:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/mapbox/satellite-v9',
+    style: 'mapbox://styles/mapbox/standard',
     accessToken: 'pk.eyJ1Ijoic2Nyb2xsaSIsImEiOiJjbHEybDh1YjcwMjgwMmtxd2Roc3J3YWt3In0.uycKijDciND-jcNknmYP1g',
     showMarkers: false,
     theme: 'light',
